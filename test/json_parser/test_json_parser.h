7768844152cd85ba994ebd5a051b66a6376fb72d
killeroonie <rob.ross@gmail.com>
2026-07-02 18:48:04 -0700

    Pulled up shared declarations.

---
+++ test/json_parser/test_json_parser.h
@@ -0,0 +1,31 @@
+#ifndef TEST_JSON_PARSER_H
+#define TEST_JSON_PARSER_H
+
+#include <gtest/gtest.h>
+#include <string>
+#include <tuple>
+
+// Wrap C library headers
+extern "C" {
+#include "roblib/arena.h" // Assuming this is where Arena is defined
+#include "roblib/json_parser.h"
+}
+
+using str_param = std::tuple<std::string, std::string>;
+
+class JsonParserEnvironment : public ::testing::Environment {
+public:
+    void SetUp() override;
+    void TearDown() override;
+
+    // The single source of truth for the arena
+    inline static Arena *arena;
+};
+
+class JsonParserTest : public testing::Test {
+protected:
+    JsonError err{};
+    Arena* arena = JsonParserEnvironment::arena;
+};
+
+#endif // TEST_JSON_PARSER_H
