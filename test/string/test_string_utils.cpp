c112cc0a58c81f225c13f27d701b61c1e3388ad3
killeroonie <rob.ross@gmail.com>
2026-07-24 23:04:42 -0700

    Renamed calls to sutil_add_to_buffer_CharRingBuffer10 as pvt_crb_add_str_to_buffer_CharRingBuffer10 commented out calls to pvt_crb_print_repr_CharRingBuffer10

--- test/string/test_string_utils.cpp
+++ test/string/test_string_utils.cpp
@@ -15,78 +15,78 @@ CHAR_RING_BUFFER(CharRingBuffer, 10);
 TEST(CharRingBuffer10, TestAdd) {
     EXPECT_EQ(sizeof(CharRingBuffer10::buffer), 10);
     CharRingBuffer10 crb{}; // Direct-list-initialization
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 
-    sutil_add_to_buffer_CharRingBuffer10(&crb, 10, "abcdefghij");
-    sutil_print_repr_CharRingBuffer10(&crb);
+    pvt_crb_add_str_to_buffer_CharRingBuffer10(&crb, 10, "abcdefghij");
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 
     crb = {};
 
     // It's good practice to verify the reset worked as expected
     EXPECT_EQ(crb.buffer[0], '\0');
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 
-    sutil_add_to_buffer_CharRingBuffer10(&crb, 5, "abcde");
+    pvt_crb_add_str_to_buffer_CharRingBuffer10(&crb, 5, "abcde");
     EXPECT_EQ(crb.length, 5);
     EXPECT_EQ(crb.end_index, 5);
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 
-    sutil_add_to_buffer_CharRingBuffer10(&crb, 10, "fghijklmno");
+    pvt_crb_add_str_to_buffer_CharRingBuffer10(&crb, 10, "fghijklmno");
     EXPECT_EQ(crb.length, 10);
     EXPECT_EQ(crb.end_index, 5);
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 }
 
 TEST(CharRingBuffer10, TestRead) {
     CharRingBuffer10 crb{}; // Direct-list-initialization
 
-    sutil_add_to_buffer_CharRingBuffer10(&crb, 10, "abcdefghij");
+    pvt_crb_add_str_to_buffer_CharRingBuffer10(&crb, 10, "abcdefghij");
     EXPECT_EQ(crb.length, 10);
     EXPECT_EQ(crb.start_index, 0);
     EXPECT_EQ(crb.end_index, 0);
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 
     // read 4 values
     for (int i = 0; i < 4; ++i) {
-        sutil_get_next_char_CharRingBuffer10(&crb);
+        pvt_crb_get_next_char_CharRingBuffer10(&crb);
     }
     EXPECT_EQ(crb.length, 6);
     EXPECT_EQ(crb.start_index, 4);
     EXPECT_EQ(crb.end_index, 0);
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 
     // read 6 values
     for (int i = 0; i < 6; ++i) {
-        sutil_get_next_char_CharRingBuffer10(&crb);
+        pvt_crb_get_next_char_CharRingBuffer10(&crb);
     }
-    EXPECT_EQ(sutil_get_next_char_CharRingBuffer10(&crb), EOF);
+    EXPECT_EQ(pvt_crb_get_next_char_CharRingBuffer10(&crb), EOF);
     EXPECT_EQ(crb.length, 0);
     EXPECT_EQ(crb.start_index, 0);
     EXPECT_EQ(crb.end_index, 0);
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 }
 
 TEST(CharRingBuffer10, TestAddReadAddRead) {
     CharRingBuffer10 crb{}; // Direct-list-initialization
-    sutil_add_to_buffer_CharRingBuffer10(&crb, 10, "abcdefghij");
+    pvt_crb_add_str_to_buffer_CharRingBuffer10(&crb, 10, "abcdefghij");
     EXPECT_EQ(crb.length, 10);
     EXPECT_EQ(crb.start_index, 0);
     EXPECT_EQ(crb.end_index, 0);
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 
     // read 6 values
     for (int i = 0; i < 6; ++i) {
-        sutil_get_next_char_CharRingBuffer10(&crb);
+        pvt_crb_get_next_char_CharRingBuffer10(&crb);
     }
     EXPECT_EQ(crb.length, 4);
     EXPECT_EQ(crb.start_index, 6);
     EXPECT_EQ(crb.end_index, 0);
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 
     // write 12 values - should only add the last 6
-    sutil_add_to_buffer_CharRingBuffer10(&crb, 12, "123456789XYZ");
+    pvt_crb_add_str_to_buffer_CharRingBuffer10(&crb, 12, "123456789XYZ");
     EXPECT_EQ(crb.length, 10);
     EXPECT_EQ(crb.start_index, 0);
     EXPECT_EQ(crb.end_index, 0);
-    sutil_print_repr_CharRingBuffer10(&crb);
+    //pvt_crb_print_repr_CharRingBuffer10(&crb);
 }
