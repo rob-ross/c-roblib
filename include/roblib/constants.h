a23ccf6ac24aca9b54d1f213b2827b2a7e8d765b
killeroonie <rob.ross@gmail.com>
2026-07-31 05:15:03 -0700

    new file for often used global constants.

---
+++ include/roblib/constants.h
@@ -0,0 +1,27 @@
+//  constants.h
+//
+//  Created by Rob Ross on 7/31/26.
+//
+//  Copyright (c) 2026.  All rights reserved.
+
+//
+
+#ifndef C_ROBLIB_CONSTANTS_H
+#define C_ROBLIB_CONSTANTS_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+constexpr size_t ONE_KIBIBYTE = 1024;
+constexpr size_t ONE_MEBIBYTE = 1024 * ONE_KIBIBYTE;
+constexpr size_t ONE_GIBIBYTE = 1024 * ONE_MEBIBYTE;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif //C_ROBLIB_CONSTANTS_H
