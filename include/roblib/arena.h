407f1e16f343654533ad14eb475597f63390424e
killeroonie <rob.ross@gmail.com>
2026-07-31 19:10:33 -0700

    Moved Arena and BlockheaderErrResult into the implementation class to treate Arena struct a opaque type. Changed signature of arena_create_arena() to no longer take an empty Arena object. added notes.

--- include/roblib/arena.h
+++ include/roblib/arena.h
@@ -7,29 +7,42 @@
 #pragma once
 
 /**
- *  Terms:
- *      Payload size:  The amount of memory requested in a call to malloc, e.g., malloc(sizeof(int))
- *      Chunk Size:    a chunk is the amount of memory requested in a malloc-like call, plus bookkeeping overhead, i.e.,
- *                     payload size + metadata header (16 bytes in 64-bit system)
- *      Page Size: the memory size used by the memory manager to allocate system memory to an allocator.
- *                  The memory manager organizes system memory into pages. It then reserves chunks from these pages
- *                  to satisfy allocation requests.
+ *  General memory management terms:
+ *      Allocation: heap allocation, dynamic allocation, explicit allocation
+ *          sometimes referred to as payload, chunk, anonymous memory
+ *
+ *      Page: The fixed-size unit (typically 4096 bytes) used by the OS to manage virtual memory.
+ *      Payload: The exact usable bytes requested by the client, excluding any allocator metadata.
+ *      Metadata / Header: The hidden bookkeeping bytes which are placed right before the payload
+ *          to store chunk size and status.
+ *      Chunk / Block: The individual, smaller segment of memory within the arena that is handed to the client user.
+ *      Free List: A data structure (often a linked list) tracking all unallocated chunks inside the arena.
+ *
+*       Allocation size: the size of an individual, normally small, memory allocation.
+ *          E.g., void * raw_ptr = malloc(10); // requesting 10 bytes, "allocating 10 bytes."
+ *
+ *      Arena: The large pool of memory (e.g., one or more 100 MB blocks) requested from the OS to be carved up.
+ *
+ *      High-Water Mark: The peak amount of total memory an allocator has claimed from the OS during execution.
+ *
  *
  *      This is a simple arena/bump allocator. It obtains system memory via calls to mmap. It manages a pointer to the
  *      next available byte of unallocated memory and increments this pointer based on the requested allocation size.
  *      It will pad the request to align it to 16 byte boundaries (via _Alignof(max_align_t)). It will grow the arena
  *      dynamically if a memory request is larger than the arena capacity. The entire arena is freed with the call to
- *      destroy_arena()
+ *      arena_destroy_arena().
  *
  *
-     void *
-     mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+ *
+ *    For POSIX we use void * mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+ *    For Windows we use malloc.
+ *
+ *    This API is not thread safe.
  */
 
 #ifndef ROBLIB_ARENA_H
 #define ROBLIB_ARENA_H
 
-#include <stdint.h>
 #include <stddef.h>
 
 #include "error_result.h"
@@ -39,38 +52,23 @@
 extern "C" {
 #endif
 
-
-typedef unsigned char byte;
-
-typedef struct block_header_t {
-    struct block_header_t * next_block;  // Links to the next memory block
-    size_t block_size;                   // Tracks size of this block for mmap
-} BlockHeader;
-
-typedef struct arena_s {
-    byte         * current_block;  // Current active memory block being filled.
-    size_t         default_block_size;     // Size of each block allocation
-    size_t         offset;         // Position inside the *current* active block (points to next available byte)
-    BlockHeader  * head_block;        // Pointer to the first block
-} Arena;
-
-
+// opaque type
+typedef struct arena_s Arena;
 
 typedef struct arena_err_result_s {
     ERROR_BASE;
     Arena * result;
 } ArenaErrResult;
 
-typedef struct block_header_err_result_s {
-    ERROR_BASE;
-    BlockHeader * result;
-} BlockHeaderErrResult;
 
 
-ArenaErrResult arena_create_arena( Arena * arena,  size_t block_size);
-void arena_destroy_arena( Arena * arena);
-void * arena_alloc(Arena * arena,  size_t size);
+
+// must call arna_destroy() when done with the Arena
+ArenaErrResult arena_create_arena( size_t arena_capacity);
 void arena_reset(Arena * arena, bool zero_mem);
+void arena_destroy_arena(const Arena * arena);
+
+void * arena_alloc(Arena * arena,  size_t size);
 
 #ifdef __cplusplus
 }
