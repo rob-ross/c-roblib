7944d955b1279f282aa81251d6fd8074c587edd2
killeroonie <rob.ross@gmail.com>
2026-08-29 00:23:23 -0700

    moved some debugging code from json_parser that might be useful in the future.

--- src/json_parser/json_parser.c
+++ src/json_parser/json_parser.c
@@ -926,24 +926,7 @@ static int pvt_hex_to_dec(const unsigned char hex)
     }
 }
 
-// 2. Printing the Bit Pattern String
-static void pvt_print_hex_bits(unsigned char hex) {
-    int val = pvt_hex_to_dec(hex);
-    if (val == -1) return;
 
-    // Extract bits using bitwise masking
-    for (int i = 3; i >= 0; i--) {
-        printf("%d", (val >> i) & 1);
-    }
-    printf("\n");
-}
-
-static void pvt_print_8_bits(uint8_t bits) {
-    printf("0b");
-    for (int i = 7; i >= 0; i--) {
-        printf("%d", (bits >> i) & 1);
-    }
-}
 
 static constexpr uint8_t continue_mask = 0b00111111;  // 0x3F
 static constexpr uint8_t continue_bits = 0b10000000;  // 0x80
